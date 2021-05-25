d42484d27b391b27a1c4a52bb9a6ec0f3691a151
Rig1234 <78023252+Rig1234@users.noreply.github.com>
2021-05-25 15:41:56 +0530

    This is the correct code for projject 28

---
+++ launcher.js
@@ -0,0 +1,34 @@
+class launcher {
+    constructor(body1,point) {
+        var options = {
+            length : 10,
+            stiffness : 0.004,
+            bodyA : body1,
+            pointB : point 
+        }
+        this.pointB = point;
+         this.SlingShot =Matter.Constraint.create(options);
+         World.add(world,this.SlingShot);
+
+
+    }
+   fly(){
+       this.launcher.bodyA = null 
+    }
+
+    display(){
+        if(this.SlingShot.bodyA ){
+    var pos1 = this.SlingShot.bodyA.position;
+    var pos2 = this.pointB;
+    push()
+    strokeWeight(5);
+    stroke("grey");
+    line(pos1.x,pos1.y,pos2.x,pos2.y);
+    pop()
+        }
+    }
+
+ 
+
+}
+
